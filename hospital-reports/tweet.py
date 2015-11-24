cbc1ef1f79c3e4b2d562dc1e17591fc247262b2a
SARA SIMON <smbsimon@gmail.com>
2015-11-24 16:01:51 -0500

    Add TypeError exception

--- hospital-reports/tweet.py
+++ hospital-reports/tweet.py
@@ -7,24 +7,28 @@ import logging
 
 twitter = Twython(consumer_key, consumer_secret, access_token, access_token_secret)
 
-with open("/home/vprnet/webapps/bots/bots/hospital-reports/past_tweets.log", "r") as f:
-    reader = f.readlines()
-    scraped_tweets = new_tweet()
-    past_tweets = []
+try:
+    with open("/home/vprnet/webapps/bots/bots/hospital-reports/past_tweets.log", "r") as f:
+        reader = f.readlines()
+        past_tweets = []
 
-    for row in reader:
-        past_tweets.append(row)
+        for row in reader:
+            past_tweets.append(row)
 
-    for report_tweet in scraped_tweets:
-        cleaned_tweet = " ".join(report_tweet)
+        scraped_tweets = new_tweet()
 
-        duplicate = [s for s in past_tweets if cleaned_tweet in s]
+        for report_tweet in scraped_tweets:
+            cleaned_tweet = " ".join(report_tweet)
 
-        if duplicate:
-            print "Already been tweeted."
-        else:
-            logging.basicConfig(filename="/home/vprnet/webapps/bots/bots/hospital-reports/past_tweets.log", level=logging.INFO)
-            logging.info(cleaned_tweet)
-            twitter.update_status(status=cleaned_tweet)
-            print cleaned_tweet
-            print "New tweet tweeted."
+            duplicate = [s for s in past_tweets if cleaned_tweet in s]
+
+            if duplicate:
+                print "Already been tweeted."
+            else:
+                logging.basicConfig(filename="/home/vprnet/webapps/bots/bots/hospital-reports/past_tweets.log", level=logging.INFO)
+                logging.info(cleaned_tweet)
+                twitter.update_status(status=cleaned_tweet)
+                print cleaned_tweet
+                print "New tweet tweeted."
+except TypeError:
+    print "No new hospital reports reported."
